634194df6bf81aa0dec91d6563f8e08eb43848f5
Priya More <priyamore8446@gmail.com>
2026-07-25 18:26:17 +0530

    Add Java program for 0-1 half pyramid pattern

---
+++ BinaryNumberhalfpyramid.java
@@ -0,0 +1,17 @@
+package Pattern_Printing;
+
+public class BinaryNumberhalfpyramid {
+    public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((i + j) % 2);
+            }
+            System.out.println();
+        }
+    }
+}
+    
+}
